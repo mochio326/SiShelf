666ae08f0496775089a92b895f115a7bd4aaa19c
MOCHIO <326mochio@gmail.com>
2017-03-30 05:45:22 +0900

    ファイル名を小文字に変更。 ドッキング時にmayaを終了すると次回起動時に復元する機能を追加

---
+++ _userSetup.py
@@ -0,0 +1,6 @@
+#mayaI—¹‚ÉSiShelf‚Ìó‘Ô‚ğ‹L˜^
+import SiShelf.shelf
+SiShelf.shelf.make_quit_app_job()
+#UI\’zŒã‚ÉSiShelf‚ğ•œŒ³
+import maya.utils
+maya.utils.executeDeferred(SiShelf.shelf.restoration_ui)
