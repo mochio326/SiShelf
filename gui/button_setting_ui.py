a85eeab783e64df1148f5d0efa2a464fe0970f37
MOCHIO <326mochio@gmail.com>
2017-03-30 05:43:36 +0900

    ファイル名を小文字に変更。 ドッキング時にmayaを終了すると次回起動時に復元する機能を追加

--- gui/button_setting_ui.py
+++ gui/button_setting_ui.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\owner\Documents\Github\SiShelf\gui\ButtonSetting.ui'
+# Form implementation generated from reading ui file 'C:\Users\owner\Documents\Github\SiShelf\gui\button_setting.ui'
 #
-# Created: Thu Mar 30 03:36:35 2017
+# Created: Thu Mar 30 05:39:45 2017
 #      by: pyside-uic 0.2.14 running on PySide 1.2.0
 #
 # WARNING! All changes made in this file will be lost!
