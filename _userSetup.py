827c6f876c5fc1a3600a8bbb489b75c51557bf5f
MOCHIO <326mochio@gmail.com>
2017-04-09 08:03:39 +0900

    配置するuserSetup.pyがutf-8じゃない場合に日本語コメントが原因で実行エラーになる場合があったので念のためコメントを英語にした。

--- _userSetup.py
+++ _userSetup.py
@@ -1,6 +1,6 @@
-#maya終了時にシェルフの状態を保存
+# Maya Save shelf state on exit
 import SiShelf.shelf
 SiShelf.shelf.make_quit_app_job()
-#ドッキング状態を起動時に復元
+# Restore docking state at startup
 import maya.utils
 maya.utils.executeDeferred(SiShelf.shelf.restoration_docking_ui)
